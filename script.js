46516cf3333e300edb94cac773fd55086409e614
Patrick LEFEVRE <patrick.lefevre@gmail.com>
2017-05-26 08:10:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,14 @@
   // Verify if querySelectorAll is supported by the browser
   if (document.querySelectorAll) {
   
-  // The variable t is an object of all HTML elements with a 'data-tooltip' attribute
+  // The variable t is an array of all HTML elements with a 'data-tooltip' attribute
     var t = document.querySelectorAll('[data-tooltip]');
     
     // Loop for all elements found
   	for (var i = 0; i < t.length; i++) {
     
       // Stores the 'title' attribute content 
-  		var o = t[i].getAttribute('title');
+      var o = t[i].getAttribute('title');
       
       // Transferts the 'title' content into a new 'aria-label' attribute attached to the current HTML element
     	t[i].setAttribute('aria-label', o);
