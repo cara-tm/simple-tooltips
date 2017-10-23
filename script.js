b7b4692be4a9486bf42099f6d9904181209af7bd
Patrick LEFEVRE <patrick.lefevre@gmail.com>
2017-10-23 10:20:21 +0200

    Update script.js
    Changes:
    * Apply new tooltips only for devices with a 680px screen width;
    * Code beauty.

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
 /*! Simple transfert for title contents into data-tooltip attributes by cara-tm.com, MIT license. */
 
-(function() {
+(function(window) {
 
-  'use strict';
+    'use strict';
 
-  // Verifies if querySelectorAll is supported by the browser
-  if (document.querySelectorAll) {
-    // Add a new class attribute within the html tag when this script is ready
-    document.querySelector('html').classList.add('tooltips');
-    
-    // The variable 't' is an array of all HTML elements with a 'data-tooltip' attribute
-    var t = document.querySelectorAll('[data-tooltip]');
-    
-    // Loop into all elements found
-  	for (var i = 0; i < t.length; i++) {
-    
-      // Stores the 'title' attribute content 
-      var o = t[i].getAttribute('title');
-      
-      // Transferts the 'title' content into a new 'aria-label' attribute attached to the current HTML element
-    	t[i].setAttribute('aria-label', o);
-      
-      // Removes the 'title' attribute into corresponding elements
-    	t[i].removeAttribute('title');
-      // Removes the 'data-tooltip' attribute into corresponding elements
-      t[i].removeAttribute('data-tooltip');
+    // Verifies if querySelectorAll is supported by the browser
+    if (document.querySelectorAll) {
+        // Add a new class attribute within the html tag when this script is ready
+        document.querySelector('html').classList.add('tooltips');
 
+        // The variable 't' is an array of all HTML elements with a 'data-tooltip' attribute
+        var t = document.querySelectorAll('[data-tooltip]');
+
+        // Loop into all elements found
+        for (var i = 0; i < t.length; i++) {
+
+            if (Math.max(document.documentElement.clientWidth, window.innerWidth || 0) || document.body.clientWidth >= 680) {
+                // Stores the 'title' attribute content 
+                var o = t[i].getAttribute('title');
+
+                // Transferts the 'title' content into a new 'aria-label' attribute attached to the current HTML element
+                t[i].setAttribute('aria-label', o);
+
+                // Removes the 'title' attribute into corresponding elements
+                t[i].removeAttribute('title');
+            }
+
+            // Removes the 'data-tooltip' attribute into corresponding elements
+            t[i].removeAttribute('data-tooltip');
+        };
     };
-  };
- 
- })();
+
+})();
